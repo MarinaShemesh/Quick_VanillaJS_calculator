3581fbb372798ca42ed1153b237caee4af354dab
MarinaShemesh <marina.shemesh1@gmail.com>
2018-02-14 23:06:27 +0200

    14feb

---
+++ script.js
@@ -0,0 +1,25 @@
+console.log("this is the js file");
+
+const inputLabel = document.getElementById('inputLabel');
+
+ function pushBtn(obj){
+   const pushed = obj.innerHTML;
+
+   if(pushed === '='){
+
+    //Calculate
+    inputLabel.innerHTML = eval(inputLabel.innerHTML);
+
+   } else if (pushed == 'AC') {
+    //all clear
+    inputLabel.innerHTML = '0';
+
+   } else {
+    if(inputLabel.innerHTML == '0') {
+       inputLabel.innerHTML = pushed;
+    } else{
+      inputLabel.innerHTML +=pushed;
+    }
+
+   }
+ }//end of pushBtn function
